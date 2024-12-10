c6ea94d9792572c7c98ef2f073c0cf910be5f620
luizakuze <luizakuze08@gmail.com>
2024-12-09 23:33:20 -0300

    auth0

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -12,9 +12,15 @@ public class HomeController {
     @RequestMapping("/")
     public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
         model.addAttribute("name", principal.getName());
-        model.addAttribute("emailAddress", principal.getFirstAttribute("email"));
+
+        // Tenta obter o email a partir dos atributos
+        String email = principal.getFirstAttribute("email");
+        if (email == null) {
+            email = principal.getFirstAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+        }
+
+        model.addAttribute("emailAddress", email);
         model.addAttribute("userAttributes", principal.getAttributes());
         return "home";
     }
-
 }
